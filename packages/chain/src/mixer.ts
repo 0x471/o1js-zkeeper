6281990542e8d70545d5382dc1ba88c17915e422
d0x471b <0x471@protonmail.com>
2023-11-17 22:20:02 +0300

    initialize mixer contract

---
+++ packages/chain/src/mixer.ts
@@ -0,0 +1,8 @@
+import {
+  RuntimeModule,
+  runtimeModule,
+  state,
+  runtimeMethod,
+} from "@proto-kit/module";
+import { State, StateMap, assert } from "@proto-kit/protocol";
+import { PublicKey, UInt64 } from "o1js";
